3a2849dd91669b808810020dbf86b7897792ce48
kechan-tesu <070busalananthess070@gmail.com>
2025-02-03 13:35:24 +0800

    4 commit

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,15 +14,15 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            ['category_name' => 'Canned Goods', 'description' => 'Various canned food items'],
-            ['category_name' => 'Dairy Products', 'description' => 'Milk, cheese, and other dairy products'],
+            ['category_name' => 'Canned goods', 'description' => 'Various canned food items'],
+            ['category_name' => 'Dairy products', 'description' => 'Milk, cheese, and other dairy products'],
             ['category_name' => 'Noodles', 'description' => 'Different types of noodles and pasta'],
-            ['category_name' => 'Frozen Foods', 'description' => 'Frozen meals and ingredients'],
+            ['category_name' => 'Frozen foods', 'description' => 'Frozen meals and ingredients'],
             ['category_name' => 'Beverages', 'description' => 'Drinks including sodas, juices, and water'],
             ['category_name' => 'Liquor', 'description' => 'Alcoholic beverages'],
             ['category_name' => 'Snacks', 'description' => 'Chips, cookies, and other snack items'],
-            ['category_name' => 'Cleaning Products', 'description' => 'Household cleaning supplies'],
-            ['category_name' => 'School and Office Supplies', 'description' => 'Stationery and office essentials'],
+            ['category_name' => 'Cleaning products', 'description' => 'Household cleaning supplies'],
+            ['category_name' => 'School and Office supplies', 'description' => 'Stationery and office essentials'],
         ];
 
         foreach ($categories as $category) {
